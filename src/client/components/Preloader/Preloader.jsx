6527e9513d192bb432b5ab7ee75b92decef0148e
Nurgazy Ismanov <nurgazy.ismanov.kk@gmail.com>
2022-09-07 19:58:11 +0600

    Updates

--- src/client/components/Preloader/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Preloader() {
-  return (
-    <div class="progress">
-      <div class="indeterminate"></div>
-    </div>
-  );
-}
--- src/client/components/reviews/index.js
+++
@@ -1,1 +0,0 @@
-export { second } from "./ReviewsList";
